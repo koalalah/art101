7aef6d68939a052890af92a8a3dd601f82ecb602
koalalah <koalalah.21@gmail.com>
2025-04-17 20:39:13 -0700

    pls.

---
+++ lab3/uehrirg.js
@@ -0,0 +1,16 @@
+//Lets psuedocode this thing: I want to add a linkn nfrom my lab 3 page to my overall page
+
+//figure out the link
+
+//create a sectiion on our webpage for putting a link
+
+//coding the link
+
+//save changes
+
+//test whether or not the link is working
+
+//publish
+
+//test
+
