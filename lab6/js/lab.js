a4c23461d4a7d74d1cdb8d47e405887ce3107eeb
koalalah <koalalah.21@gmail.com>
2025-04-28 21:07:05 -0700

    final lab 6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -28,14 +28,13 @@ var myMainRide = {
   model: "Accord",
   color: "Lunar Silver",
   year: 2017,
-  age: function () {
-    return 2025-2017;
+  age:function() {
+    return 2025 - this.year;
   }
 };
-
-console.log(myMainRide.age());
+console.log(myMainRide.age())
 
 document.open();
 document.writeln("Kinds of transportation I use: ", myTransport, "<br>");
-document.writeln("My Main Ride: </pre>");
-document.writeln(JSON.stringify(myMainRide, null, '\t'));
+document.writeln("My Main Ride: </pre>",
+  JSON.stringify(myMainRide, null, '\t'), "</pre>");
