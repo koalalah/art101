e4d05add63dc828a6d9ad3aec20b16afd82ec28d
koalalah <koalalah.21@gmail.com>
2025-04-28 20:39:29 -0700

    lab 6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -7,15 +7,35 @@
 // Functions
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+// function myFunction(param1, param2) {
+//   // some code here
+//   // return results;
+// }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+// function main() {
+//   console.log("Main function started.");
+//   // the code that makes everything happen
+// }
 
-// let's get this party started
-main();
+// // let's get this party started
+// main();
+
+var myTransport = ["Honda Accord", " bus", " lightrail", " friends' vehicles"];
+console.log("My Transportations: " + myTransport)
+
+var myMainRide = {
+  make: "Honda",
+  model: "Accord",
+  color: "Lunar Silver",
+  year: 2017,
+  age: function () {
+    return 2025-2017;
+  }
+};
+
+console.log(myMainRide.age());
+
+document.open();
+document.writeln("Kinds of transportation I use: ", myTransport, "<br>");
+document.writeln("My Main Ride: </pre>");
+document.writeln(JSON.stringify(myMainRide, null, '\t'));
