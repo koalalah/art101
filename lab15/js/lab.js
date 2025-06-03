6d664221a73c91c16a5b081c1105168c9653f76b
koalalah <koalalah.21@gmail.com>
2025-06-02 20:34:35 -0700

    lab15

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -31,18 +31,14 @@ Date: May 12 202
 
 // Using the core $.ajax() method
 $.ajax({
-    // The URL for the request (from the api docs)
+
     url: "https://yesno.wtf/api",
-    // The data to send (will be converted to a query string)
     data: { 
            
           },
-    // Whether this is a POST or GET request
     type: "GET",
-    // The type of data we expect back
     dataType : "json",
-    // What do we do when the api call is successful
-    //   all the action goes in here
+
     success: function(data) {
         // do stuff
         console.log(data);
@@ -50,7 +46,7 @@ $.ajax({
         $("#output").append("<img src="+data.image+">");
 
     },
-    // What we do if the api call fails
+
     error: function (jqXHR, textStatus, errorThrown) { 
         // do stuff
         console.log("Error:", textStatus, errorThrown);
